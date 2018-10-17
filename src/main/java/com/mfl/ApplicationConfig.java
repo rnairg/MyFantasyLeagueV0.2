3f5b94d4f0efe090fa87a4c41fceba9c01313736
Rakesh Nair <rnairg@gmail.com>
2018-10-17 22:02:32 +0530

    Removed XML based config for Hibernate and added Java based
    Config.Implemented Spring Profiling with profile as dev.

---
+++ src/main/java/com/mfl/ApplicationConfig.java
@@ -0,0 +1,57 @@
+package com.mfl;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@Configuration
+public class ApplicationConfig {
+	
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public LocalSessionFactoryBean getSessionFactory()
+	{
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(getDataSource());
+		sessionFactory.setPackagesToScan(new String[] {"com.mfl.models"});
+		sessionFactory.setHibernateProperties(getHibernateProperties());
+		return sessionFactory;
+	}
+
+	@Bean	
+	public Properties getHibernateProperties() {
+		return new Properties() {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = -3938700260142429593L;
+
+			{
+				setProperty("hibernate.hbm2ddl.auto",
+			    env.getProperty("spring.jpa.hibernate.ddl-auto"));
+			    setProperty("hibernate.dialect",
+			    env.getProperty("spring.jpa.properties.hibernate.dialect"));
+			}
+			
+		};
+	}
+
+	@Bean	
+	public DataSource getDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+		dataSource.setUrl(env.getProperty("spring.datasource.url"));
+		dataSource.setUsername(env.getProperty("spring.datasource.username"));
+		dataSource.setPassword(env.getProperty("spring.datasource.password"));
+		return dataSource;
+	}
+}
--- src/main/java/com/mfl/HomeController.java
+++
@@ -1,8 +0,0 @@
-package com.mfl;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HomeController {
-
-}
